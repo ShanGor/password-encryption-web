c4ec9e5f14077589bf692cfa96eb3afd01bb0742
sam <samuelchan@qq.com>
2020-02-18 22:06:20 +0800

    Initial commit

---
+++ src/main/java/tech/comfortheart/PasswordEncryptionController.java
@@ -0,0 +1,27 @@
+package tech.comfortheart;
+
+import org.apache.tomcat.util.buf.HexUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import tech.comfortheart.util.EncryptUtil;
+
+@Controller
+public class PasswordEncryptionController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/encrypt")
+    @ResponseBody public String encrypt(@RequestBody String plainText) {
+        try {
+            App.KeystoreAndCert keyInfo = App.getKeystoreAndCert();
+            return EncryptUtil.encrypt(keyInfo.getCertPath(), plainText);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
